10846a3824a1383f0b87037066474f70f081fb9a
Fedescalda <fedescaldaferro@gmail.com>
2023-07-06 18:01:49 -0300

    solution all errors

--- script.js
+++ script.js
@@ -1,36 +1,41 @@
 function ShowTopicField() {
     let invitationSelect = document.getElementById("invitation");
     let themeCamp = document.getElementById("theme-camp");
-
+  
     if (invitationSelect.value === "party" || invitationSelect.value === "wedding" || invitationSelect.value === "meeting") {
       themeCamp.style.display = "block";
     } else {
       themeCamp.style.display = "none";
     }
   }
-  // Obtener los valores de los campos del formulario en index.html
-  const name = document.getElementById("name").value;
-  const address = document.getElementById("address").value;
-  const date = document.getElementById("date").value;
-  const time = document.getElementById("time").value;
-  const cel = document.getElementById("cel").value;
-  const invitation = document.getElementById("invitation").value;
-  const theme = document.getElementById("theme").value;
-
-  // Mostrar los valores en invitation.html
-  document.getElementById("name").textContent = name;
-  document.getElementById("address").textContent = address;
-  document.getElementById("date").textContent = date;
-  document.getElementById("time").textContent = time;
-  document.getElementById("cel").textContent = cel;
-  document.getElementById("invitation").textContent = invitation;
-  document.getElementById("theme").textContent = theme;
-function confirmAttendance() {
-    // Aquí puedes implementar la lógica para confirmar la asistencia, como enviar los datos al servidor o actualizar el estado en una base de datos.
-    // También puedes mostrar un mensaje de confirmación al usuario.
-}
-
-function rejectAttendance() {
-    // Aquí puedes implementar la lógica para rechazar la asistencia, como enviar los datos al servidor o actualizar el estado en una base de datos.
-    // También puedes mostrar un mensaje de confirmación al usuario.
-}
+  
+  function generateInvitation() {
+    // Obtener los valores de los campos del formulario en index.html
+    const name = document.getElementById("name").value;
+    const address = document.getElementById("address").value;
+    const date = document.getElementById("date").value;
+    const time = document.getElementById("time").value;
+    const theme = document.getElementById("theme").value;
+  
+    // Redireccionar a invitation.html con los parámetros de la invitación
+    window.location.href = `invitation.html?name=${encodeURIComponent(name)}&address=${encodeURIComponent(address)}&date=${encodeURIComponent(date)}&time=${encodeURIComponent(time)}&cel=${encodeURIComponent(cel)}&invitation=${encodeURIComponent(invitation)}&theme=${encodeURIComponent(theme)}`;
+  }
+  
+  document.addEventListener("DOMContentLoaded", function() {
+    // Obtener los valores de la URL en invitation.html
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const nameParam = urlParams.get('name');
+    const addressParam = urlParams.get('address');
+    const dateParam = urlParams.get('date');
+    const timeParam = urlParams.get('time');
+    const themeParam = urlParams.get('theme');
+  
+    // Mostrar los valores en invitation.html
+    document.getElementById("name-value").textContent = nameParam;
+    document.getElementById("address-value").textContent = addressParam;
+    document.getElementById("date-value").textContent = dateParam;
+    document.getElementById("time-value").textContent = timeParam;
+    document.getElementById("theme-value").textContent = themeParam;
+  });
+  
